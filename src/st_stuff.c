75f72ac5705e8b3a29e84e29fcad396d80c13752
Jaime Passos <lazymyuutsu@gmail.com>
2020-03-26 22:01:05 -0300

    Hide touch inputs in demos

--- src/st_stuff.c
+++ src/st_stuff.c
@@ -3185,7 +3185,7 @@ static void ST_overlayDrawer(void)
 	if ((cv_showinput.value && !players[displayplayer].spectator) || (modeattacking && !(demoplayback && hu_showscores)))
 		ST_drawInput();
 #ifdef TOUCHINPUTS
-	else if (stplyr == &players[consoleplayer])
+	else if (stplyr == &players[consoleplayer] && (!demoplayback))
 		ST_drawTouchGameInput();
 #endif
 
