3a9fd691970b4caa017694adec8b2fe16eea7294
romoney5 <93959424+romoney5@users.noreply.github.com>
2025-12-15 15:42:54 -0500

    fix crashing😱😱😱
    i think
    
    two fields were being set incorrectly in COM_AddLuaCommand so replaced was being uninitialized

--- src/command.c
+++ src/command.c
@@ -568,7 +568,8 @@ int COM_AddLuaCommand(const char *name)
 	cmd->function = COM_Lua_f;
 	cmd->flags = COM_LUA;
 	cmd->next = com_commands;
-	cmd->luacmd = false;
+	cmd->replaced = false;
+	cmd->luacmd = true;
 	com_commands = cmd;
 	return 0;
 }
--- src/g_game.c
+++ src/g_game.c
@@ -2300,7 +2300,7 @@ void G_Ticker(boolean run)
 	INT32 buf;
 
 	// Bot players queued for removal
-	for (i = MAXPLAYERS-1; i != UINT32_MAX; i--)
+	for (i = MAXPLAYERS-1; i <= MAXPLAYERS-1; i--)
 	{
 		if (playeringame[i] && players[i].removing)
 		{
