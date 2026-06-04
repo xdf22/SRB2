de2b2f529946a70900f6340f78ea6e5b6beea9ad
xdf22 <nothingjust752@gmail.com>
2026-06-04 19:10:41 +0300

    increase sprite limit lol

--- src/r_things.h
+++ src/r_things.h
@@ -120,7 +120,7 @@ void R_DrawMasked(maskcount_t* masks, INT32 nummasks);
 
 // number of sprite lumps for spritewidth,offset,topoffset lookup tables
 // Fab: this is a hack : should allocate the lookup tables per sprite
-#define MAXVISSPRITES 2048 // added 2-2-98 was 128
+#define MAXVISSPRITES 99999 // added 2-2-98 was 128
 
 #define VISSPRITECHUNKBITS 6	// 2^6 = 64 sprites per chunk
 #define VISSPRITESPERCHUNK (1 << VISSPRITECHUNKBITS)
