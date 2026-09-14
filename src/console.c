SRB2-Mobile/SRB2-Android

Many things: * Added menu button * Added pause button * Added talk (and team talk) button * Added F12 button * Added console button (in-game) * Chat brings up the keyboard * IPv4 textbox properly brings up the keyboard * Some messages now refer to the touch screen instead of the keyboard * Other changes I already forgot at this point

src/console.c
@@ -100,6 +100,10 @@ static void CON_ChangeHeight(void);
 static void CONS_hudlines_Change(void);
 static void CONS_backcolor_Change(void);
 
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+static void CON_ScreenKeyboardInput(char *text, size_t length);
+#endif
+
 //======================================================================
 //                   CONSOLE VARS AND COMMANDS
 //======================================================================
@@ -401,6 +405,10 @@ void CON_Init(void)
 	con_destlines = vid.height;
 	con_curlines = vid.height;
 
+#if defined(__ANDROID__)
+	// Lactozilla: Change the default console height for Android
+	cons_height.defaultvalue = "40";
+#endif
 
 	if (!dedicated)
 	{
@@ -602,6 +610,11 @@ boolean CON_Ready(void)
 	return consoleready;
 }
 
+void CON_Toggle(void)
+{
+	consoletoggle = true;
+}
+
 // Console ticker: handles console move in/out, cursor blinking
 //
 void CON_Ticker(void)
@@ -612,6 +625,12 @@ void CON_Ticker(void)
 	// cursor blinking
 	con_tick++;
 	con_tick &= 7;
+
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+	// Lactozilla: Close the console, if the screen keyboard is not visible
+	if (consoleready && (!I_KeyboardOnScreen()))
+		consoletoggle = true;
+#endif
 
 	// console key was pushed
 	if (consoletoggle)
@@ -626,7 +645,12 @@ void CON_Ticker(void)
 			I_UpdateMouseGrab();
 		}
 		else
+		{
 			CON_ChangeHeight();
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+			con_scrollup = 0;
+#endif
+		}
 	}
 
 	// console movement
@@ -647,7 +671,17 @@ void CON_Ticker(void)
 
 	// check if console ready for prompt
 	if (con_destlines >= minheight)
+	{
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+		// Lactozilla: Raise the screen keyboard
+		if (!I_KeyboardOnScreen())
+		{
+			I_RaiseScreenKeyboard(NULL, 0);
+			I_ScreenKeyboardCallback(CON_ScreenKeyboardInput);
+		}
+#endif
 		consoleready = true;
+	}
 	else
 		consoleready = false;
 
@@ -735,6 +769,14 @@ static void CON_InputDelChar(void)
 	inputlines[inputline][--input_len] = 0;
 	input_sel = --input_cur;
 }
+
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+static void CON_ScreenKeyboardInput(char *text, size_t length)
+{
+	(void)length;
+	CON_InputAddString(text);
+}
+#endif
 
 //
 // ----
@@ -917,7 +959,7 @@ boolean CON_Responder(event_t *ev)
 
 		// ...why shouldn't it eat the key? if it doesn't, it just means you
 		// can control Sonic from the console, which is silly
-		return true;//return false;
+		return true;
 	}
 
 	// command completion forward (tab) and backward (shift-tab)
@@ -1080,6 +1122,12 @@ boolean CON_Responder(event_t *ev)
 			CON_InputSetString(inputlines[inputhist]);
 		return true;
 	}
+
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+	// Inputs handled elsewhere
+	if (I_KeyboardOnScreen())
+		return true;
+#endif
 
 	// allow people to use keypad in console (good for typing IP addresses) - Calum
 	if (key >= KEY_KEYPAD7 && key <= KEY_KPADDEL)

src/hu_stuff.c
@@ -922,7 +922,24 @@ static void Got_Saycmd(UINT8 **p, INT32 playernum)
 			fmt2 = "%s<%s%s>\x80%s %s%s";
 		}
 
-		HU_AddChatText(va(fmt2, prefix, cstart, dispname, cend, textcolor, msg), cv_chatnotifications.value); // add to chat
+		// add to chat
+#if defined(__ANDROID__)
+		{
+			// Lactozilla: va() in Android doesn't like color codes
+			size_t length = strlen(prefix) + strlen(cstart) + strlen(dispname) + strlen(cend) + strlen(textcolor) + strlen(msg);
+			char *finalstring = Z_Malloc(length + 5, PU_STATIC, NULL);
+			finalstring[0] = '\0';
+#define cat(x) strcat(finalstring, x)
+			cat(prefix); cat("<"); cat(cstart); cat(dispname); cat(">\x80");
+			cat(cend); cat(" ");
+			cat(textcolor); cat(msg);
+#undef cat
+			HU_AddChatText(finalstring, cv_chatnotifications.value);
+			Z_Free(finalstring);
+		}
+#else
+		HU_AddChatText(va(fmt2, prefix, cstart, dispname, cend, textcolor, msg), cv_chatnotifications.value);
+#endif
 
 		if (tempchar)
 			Z_Free(tempchar);
@@ -941,6 +958,7 @@ static void Got_Saycmd(UINT8 **p, INT32 playernum)
 //
 static inline boolean HU_keyInChatString(char *s, char ch)
 {
+#if (!(defined(__ANDROID__) && defined(TOUCHINPUTS)))
 	size_t l;
 
 	if ((ch >= HU_FONTSTART && ch <= HU_FONTEND && hu_font[ch-HU_FONTSTART])
@@ -974,7 +992,9 @@ static inline boolean HU_keyInChatString(char *s, char ch)
 		}
 		return false;
 	}
-	else if (ch == KEY_BACKSPACE)
+	else
+#endif
+	if (ch == KEY_BACKSPACE)
 	{
 		size_t i = c_input;
 
@@ -1019,6 +1039,12 @@ void HU_Ticker(void)
 		hu_showscores = !chat_on;
 	else
 		hu_showscores = false;
+
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+	// Close the chat if the keyboard isn't visible
+	if (chat_on && (!I_KeyboardOnScreen()))
+		HU_CloseChat();
+#endif
 }
 
 #ifndef NONET
@@ -1160,6 +1186,9 @@ void HU_clearChatChars(void)
 	c_input = 0;
 
 	I_UpdateMouseGrab();
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+	I_CloseScreenKeyboard();
+#endif
 }
 
 #ifndef NONET
@@ -1212,21 +1241,14 @@ boolean HU_Responder(event_t *ev)
 		if ((ev->data1 == gamecontrol[gc_talkkey][0] || ev->data1 == gamecontrol[gc_talkkey][1])
 			&& netgame && !OLD_MUTE) // check for old chat mute, still let the players open the chat incase they want to scroll otherwise.
 		{
-			chat_on = true;
-			w_chat[0] = 0;
+			HU_OpenChat();
 			teamtalk = false;
-			chat_scrollmedown = true;
-			typelines = 1;
 			return true;
 		}
 		if ((ev->data1 == gamecontrol[gc_teamkey][0] || ev->data1 == gamecontrol[gc_teamkey][1])
 			&& netgame && !OLD_MUTE)
 		{
-			chat_on = true;
-			w_chat[0] = 0;
-			teamtalk = G_GametypeHasTeams(); // Don't teamtalk if we don't have teams.
-			chat_scrollmedown = true;
-			typelines = 1;
+			HU_OpenChat();
 			return true;
 		}
 	}
@@ -1256,6 +1278,7 @@ boolean HU_Responder(event_t *ev)
 				c = shiftxform[c];
 		}
 
+#if (!(defined(__ANDROID__) && defined(TOUCHINPUTS)))
 		// pasting. pasting is cool. chat is a bit limited, though :(
 		if (((c == 'v' || c == 'V') && ctrldown) && !CHAT_MUTE)
 		{
@@ -1300,6 +1323,10 @@ boolean HU_Responder(event_t *ev)
 				return true;
 			}
 		}
+#else
+		// Lactozilla: Force the cursor to always be at the end of the text
+		c_input = strlen(w_chat);
+#endif
 
 		if (!CHAT_MUTE && HU_keyInChatString(w_chat,c))
 		{
@@ -1307,19 +1334,15 @@ boolean HU_Responder(event_t *ev)
 		}
 		if (c == KEY_ENTER)
 		{
-			chat_on = false;
-			c_input = 0; // reset input cursor
+			HU_CloseChat();
 			chat_scrollmedown = true; // you hit enter, so you might wanna autoscroll to see what you just sent. :)
-			I_UpdateMouseGrab();
 		}
 		else if (c == KEY_ESCAPE
 			|| ((c == gamecontrol[gc_talkkey][0] || c == gamecontrol[gc_talkkey][1]
 			|| c == gamecontrol[gc_teamkey][0] || c == gamecontrol[gc_teamkey][1])
 			&& c >= KEY_MOUSE1)) // If it's not a keyboard key, then the chat button is used as a toggle.
 		{
-			chat_on = false;
-			c_input = 0; // reset input cursor
-			I_UpdateMouseGrab();
+			HU_CloseChat();
 		}
 		else if ((c == KEY_UPARROW || c == KEY_MOUSEWHEELUP) && chat_scroll > 0 && !OLDCHAT) // CHAT SCROLLING YAYS!
 		{
@@ -1358,6 +1381,41 @@ boolean HU_Responder(event_t *ev)
 //======================================================================
 //                         HEADS UP DRAWING
 //======================================================================
+
+void HU_OpenChat(void)
+{
+#ifndef NONET
+	chat_on = true;
+	w_chat[0] = 0;
+	teamtalk = G_GametypeHasTeams(); // Don't teamtalk if we don't have teams.
+	chat_scrollmedown = true;
+	typelines = 1;
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+	I_RaiseScreenKeyboard(w_chat, HU_MAXMSGLEN);
+#endif
+#endif
+}
+
+void HU_CloseChat(void)
+{
+#ifndef NONET
+	chat_on = false;
+	c_input = 0; // reset input cursor
+	I_UpdateMouseGrab();
+#if (defined(__ANDROID__) && defined(TOUCHINPUTS))
+	I_CloseScreenKeyboard();
+#endif
+#endif
+}
+
+boolean HU_IsChatOpen(void)
+{
+#ifndef NONET
+	return chat_on;
+#else
+	return false;
+#endif
+}
 
 #ifndef NONET
 

src/sdl/i_system.c
@@ -843,6 +843,23 @@ INT32 I_GetKey (void)
 	return rc;
 }
 
+#ifdef TOUCHINPUTS
+void I_GetFinger(INT32 *x, INT32 *y)
+{
+	event_t *ev;
+	for (; eventtail != eventhead; eventtail = (eventtail+1)&(MAXEVENTS-1))
+	{
+		ev = &events[eventtail];
+		if (ev->type == ev_touchdown || ev->type == ev_touchmotion)
+		{
+			*x = ev->data1;
+			*y = ev->data2;
+			continue;
+		}
+	}
+}
+#endif
+
 //
 // I_JoyScale
 //

xdf22/SRB2

src/console.c
@@ -104,6 +104,10 @@ static void CONS_height_Change(void);
 static void CONS_hudlines_Change(void);
 static void CONS_backcolor_Change(void);
 
+#if (defined(IOS) && defined(TOUCHINPUTS))
+static void CON_ScreenKeyboardInput(char *text, size_t length);
+#endif
+
 //======================================================================
 //                   CONSOLE VARS AND COMMANDS
 //======================================================================
@@ -480,6 +484,11 @@ void CON_Init(void)
 	con_destlines = vid.height;
 	con_curlines = vid.height;
 
+#if defined(IOS)
+	// Lactozilla: Change the default console height for Android
+	cons_height.defaultvalue = "40";
+#endif
+
 	Unlock_state();
 
 	if (!dedicated)
@@ -755,6 +764,11 @@ boolean CON_Ready(void)
 	return ready;
 }
 
+void CON_Toggle(void)
+{
+	consoletoggle = true;
+}
+
 // Console ticker: handles console move in/out, cursor blinking
 //
 void CON_Ticker(void)
@@ -770,6 +784,12 @@ void CON_Ticker(void)
 	con_tick++;
 	con_tick &= 7;
 
+#if (defined(IOS) && defined(TOUCHINPUTS))
+	// Lactozilla: Close the console, if the screen keyboard is not visible
+	if (consoleready && (!I_KeyboardOnScreen()))
+		consoletoggle = true;
+#endif
+
 	// console key was pushed
 	if (consoletoggle)
 	{
@@ -783,12 +803,27 @@ void CON_Ticker(void)
 			I_UpdateMouseGrab();
 		}
 		else
+		{
 			CON_ChangeHeight();
+#if (defined(IOS) && defined(TOUCHINPUTS))
+			con_scrollup = 0;
+#endif
+		}
 	}
 
 	// check if console ready for prompt
 	if (con_destlines >= minheight)
+	{
+#if (defined(IOS) && defined(TOUCHINPUTS))
+		// Lactozilla: Raise the screen keyboard
+		if (!I_KeyboardOnScreen())
+		{
+			I_RaiseScreenKeyboard(NULL, 0);
+			I_ScreenKeyboardCallback(CON_ScreenKeyboardInput);
+		}
+#endif
 		consoleready = true;
+	}
 	else
 		consoleready = false;
 
@@ -908,6 +943,14 @@ static void CON_InputDelChar(void)
 
 	Unlock_state();
 }
+
+#if (defined(IOS) && defined(TOUCHINPUTS))
+static void CON_ScreenKeyboardInput(char *text, size_t length)
+{
+	(void)length;
+	CON_InputAddString(text);
+}
+#endif
 
 //
 // ----
@@ -1124,7 +1167,7 @@ boolean CON_Responder(event_t *ev)
 
 		// ...why shouldn't it eat the key? if it doesn't, it just means you
 		// can control Sonic from the console, which is silly
-		return true;//return false;
+		return true;
 	}
 
 	// command completion forward (tab) and backward (shift-tab)
@@ -1285,6 +1328,12 @@ boolean CON_Responder(event_t *ev)
 			CON_InputSetString(inputlines[inputhist]);
 		return true;
 	}
+
+#if (defined(IOS) && defined(TOUCHINPUTS))
+	// Inputs handled elsewhere
+	if (I_KeyboardOnScreen())
+		return true;
+#endif
 
 	// allow people to use keypad in console (good for typing IP addresses) - Calum
 	if (key >= KEY_KEYPAD7 && key <= KEY_KPADDEL)

src/hu_stuff.c
@@ -824,7 +824,24 @@ static void Got_Saycmd(UINT8 **p, INT32 playernum)
 			fmt2 = "%s<%s%s>\x80%s %s%s";
 		}
 
-		HU_AddChatText(va(fmt2, prefix, cstart, dispname, cend, textcolor, msg), cv_chatnotifications.value); // add to chat
+		// add to chat
+#if defined(__ANDROID__)
+		{
+			// Lactozilla: va() in Android doesn't like color codes
+			size_t length = strlen(prefix) + strlen(cstart) + strlen(dispname) + strlen(cend) + strlen(textcolor) + strlen(msg);
+			char *finalstring = Z_Malloc(length + 5, PU_STATIC, NULL);
+			finalstring[0] = '\0';
+#define cat(x) strcat(finalstring, x)
+			cat(prefix); cat("<"); cat(cstart); cat(dispname); cat(">\x80");
+			cat(cend); cat(" ");
+			cat(textcolor); cat(msg);
+#undef cat
+			HU_AddChatText(finalstring, cv_chatnotifications.value);
+			Z_Free(finalstring);
+		}
+#else
+		HU_AddChatText(va(fmt2, prefix, cstart, dispname, cend, textcolor, msg), cv_chatnotifications.value);
+#endif
 
 		if (tempchar)
 			Z_Free(tempchar);
@@ -872,6 +889,12 @@ void HU_Ticker(void)
 		hu_showscores = !chat_on;
 	else
 		hu_showscores = false;
+
+#if (defined(IOS) && defined(TOUCHINPUTS))
+	// Close the chat if the keyboard isn't visible
+	if (chat_on && (!I_KeyboardOnScreen()))
+		HU_CloseChat();
+#endif
 
 	if (chat_on)
 	{
@@ -1018,6 +1041,9 @@ void HU_clearChatChars(void)
 	c_input = 0;
 
 	I_UpdateMouseGrab();
+#if (defined(IOS) && defined(TOUCHINPUTS))
+	I_CloseScreenKeyboard();
+#endif
 }
 
 //
@@ -1063,25 +1089,14 @@ boolean HU_Responder(event_t *ev)
 		if ((ev->key == gamecontrol[GC_TALKKEY][0] || ev->key == gamecontrol[GC_TALKKEY][1])
 			&& netgame && !OLD_MUTE) // check for old chat mute, still let the players open the chat incase they want to scroll otherwise.
 		{
-			I_SetTextInputMode(true);
-			chat_on = true;
-			chat_on_first_event = false;
-			w_chat[0] = 0;
+			HU_OpenChat();
 			teamtalk = false;
-			chat_scrollmedown = true;
-			typelines = 1;
 			return true;
 		}
 		if ((ev->key == gamecontrol[GC_TEAMKEY][0] || ev->key == gamecontrol[GC_TEAMKEY][1])
 			&& netgame && !OLD_MUTE)
 		{
-			I_SetTextInputMode(true);
-			chat_on = true;
-			chat_on_first_event = false;
-			w_chat[0] = 0;
-			teamtalk = G_GametypeHasTeams(); // Don't teamtalk if we don't have teams.
-			chat_scrollmedown = true;
-			typelines = 1;
+			HU_OpenChat();
 			return true;
 		}
 	}
@@ -1121,6 +1136,7 @@ boolean HU_Responder(event_t *ev)
 		 || ev->key == KEY_LALT || ev->key == KEY_RALT)
 			return true;
 
+#if (!(defined(IOS) && defined(TOUCHINPUTS)))
 		// pasting. pasting is cool. chat is a bit limited, though :(
 		if (c == 'v' && ctrldown)
 		{
@@ -1145,7 +1161,11 @@ boolean HU_Responder(event_t *ev)
 			c_input += pastelen;
 			return true;
 		}
-		else if (c == KEY_ENTER)
+#else
+		// Lactozilla: Force the cursor to always be at the end of the text
+		c_input = strlen(w_chat);
+#endif
+		if (c == KEY_ENTER)
 		{
 			if (!CHAT_MUTE)
 				HU_sendChatMessage();
@@ -1154,17 +1174,13 @@ boolean HU_Responder(event_t *ev)
 			chat_on = false;
 			c_input = 0; // reset input cursor
 			chat_scrollmedown = true; // you hit enter, so you might wanna autoscroll to see what you just sent. :)
-			I_UpdateMouseGrab();
 		}
 		else if (c == KEY_ESCAPE
 			|| ((c == gamecontrol[GC_TALKKEY][0] || c == gamecontrol[GC_TALKKEY][1]
 			|| c == gamecontrol[GC_TEAMKEY][0] || c == gamecontrol[GC_TEAMKEY][1])
 			&& c >= KEY_MOUSE1)) // If it's not a keyboard key, then the chat button is used as a toggle.
 		{
-			I_SetTextInputMode(false);
-			chat_on = false;
-			c_input = 0; // reset input cursor
-			I_UpdateMouseGrab();
+			HU_CloseChat();
 		}
 		else if ((c == KEY_UPARROW || c == KEY_MOUSEWHEELUP) && chat_scroll > 0 && !OLDCHAT) // CHAT SCROLLING YAYS!
 		{
@@ -1214,6 +1230,40 @@ boolean HU_Responder(event_t *ev)
 	return false;
 }
 
+void HU_OpenChat(void)
+{
+#ifndef NONET
+	chat_on = true;
+	w_chat[0] = 0;
+	teamtalk = G_GametypeHasTeams(); // Don't teamtalk if we don't have teams.
+	chat_scrollmedown = true;
+	typelines = 1;
+#if (defined(IOS) && defined(TOUCHINPUTS))
+	I_RaiseScreenKeyboard(w_chat, HU_MAXMSGLEN);
+#endif
+#endif
+}
+
+void HU_CloseChat(void)
+{
+#ifndef NONET
+	chat_on = false;
+	c_input = 0; // reset input cursor
+	I_UpdateMouseGrab();
+#if (defined(IOS) && defined(TOUCHINPUTS))
+	I_CloseScreenKeyboard();
+#endif
+#endif
+}
+
+boolean HU_IsChatOpen(void)
+{
+#ifndef NONET
+	return chat_on;
+#else
+	return false;
+#endif
+}
 
 //======================================================================
 //                         HEADS UP DRAWING

src/sdl/i_system.c
@@ -1031,6 +1031,23 @@ INT32 I_GetKey (void)
 	return rc;
 }
 
+#ifdef TOUCHINPUTS
+void I_GetFinger(INT32 *x, INT32 *y)
+{
+	event_t *ev;
+	for (; eventtail != eventhead; eventtail = (eventtail+1)&(MAXEVENTS-1))
+	{
+		ev = &events[eventtail];
+		if (ev->type == ev_touchdown || ev->type == ev_touchmotion)
+		{
+			*x = ev->x;
+			*y = ev->y;
+			continue;
+		}
+	}
+}
+#endif
+
 //
 // I_JoyScale
 //