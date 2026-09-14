SRB2-Mobile/SRB2-Android

Change the design of the jump / spin buttons yet again

src/g_input.c
@@ -1267,12 +1267,14 @@ static void G_DefineTouchGameControls(void)
 		touchcontrols[gc_straferight].h = 14;
 
 		// Jump
+		touchcontrols[gc_jump].name = "JMP";
 		touchcontrols[gc_jump].w = 40;
 		touchcontrols[gc_jump].h = 32;
 		touchcontrols[gc_jump].x = ((vid.width / vid.dupx) - touchcontrols[gc_jump].w - corneroffset - 12);
 		touchcontrols[gc_jump].y = ((vid.height / vid.dupy) - touchcontrols[gc_jump].h - corneroffset - 12) + offs;
 
 		// Spin
+		touchcontrols[gc_use].name = "SPN";
 		touchcontrols[gc_use].w = 32;
 		touchcontrols[gc_use].h = 24;
 		touchcontrols[gc_use].x = (touchcontrols[gc_jump].x - touchcontrols[gc_use].w - 12);
@@ -1320,13 +1322,15 @@ static void G_DefineTouchGameControls(void)
 		touchcontrols[gc_straferight].h = 28;
 
 		// Jump
+		touchcontrols[gc_jump].name = "JUMP";
 		touchcontrols[gc_jump].w = 48;
 		touchcontrols[gc_jump].h = 48;
 		touchcontrols[gc_jump].x = ((vid.width / vid.dupx) - touchcontrols[gc_jump].w - corneroffset - 12);
 		touchcontrols[gc_jump].y = ((vid.height / vid.dupy) - touchcontrols[gc_jump].h - corneroffset - 12) + offs;
 
 		// Spin
-		touchcontrols[gc_use].w = 32;
+		touchcontrols[gc_use].name = "SPIN";
+		touchcontrols[gc_use].w = 40;
 		touchcontrols[gc_use].h = 32;
 		touchcontrols[gc_use].x = (touchcontrols[gc_jump].x - touchcontrols[gc_use].w - 12);
 		touchcontrols[gc_use].y = touchcontrols[gc_jump].y + 12;

src/st_stuff.c
@@ -1441,9 +1441,10 @@ void ST_drawTouchGameInput(boolean drawgamecontrols)
 	}
 
 #define DEFAULTKEYCOL 16 // Because of macro expansion, this define needs to be up here.
-#define drawbutton(gctype, butt, symb, strxoffs, stryoffs, keycol) { \
+#define drawbutton(gctype, butt, str, strxoffs, stryoffs, keycol) { \
 	touchconfig_t *control = &touchcontrols[gctype]; \
 	if (!control->hidden) { \
+		const char *keystr = ((str == NULL) ? control->name : str); \
 		SCALEBUTTON(control); \
 		if ((butt != 0 && (stplyr->cmd.buttons & butt)) \
 		|| control->pressed > I_GetTime()) \
@@ -1458,21 +1459,21 @@ void ST_drawTouchGameInput(boolean drawgamecontrols)
 			V_DrawFill(x, y + h, w, shadow, 29|flags); \
 		} \
 		V_DrawFill(x, y + offs, w, h, col|flags); \
-		V_DrawString((x + (w / 2)) - (V_StringWidth(symb, flags) / 2) + strxoffs, \
+		V_DrawString((x + (w / 2)) - (V_StringWidth(keystr, flags) / 2) + strxoffs, \
 					((y + (h / 2)) - ((8*vid.dupy) / 2) + offs) + stryoffs, \
-					flags, symb); \
+					flags, keystr); \
 		} \
 	}
 
-#define drawbutt(gctype, butt, symb) drawbutton(gctype, butt, symb, 0, 0, DEFAULTKEYCOL)
-#define drawcolbutt(gctype, butt, symb, col) drawbutton(gctype, butt, symb, 0, 0, col)
-#define drawoffsbutt(gctype, butt, symb, xoffs, yoffs) drawbutton(gctype, butt, symb, xoffs, yoffs, DEFAULTKEYCOL)
+#define drawbutt(gctype, butt, str) drawbutton(gctype, butt, str, 0, 0, DEFAULTKEYCOL)
+#define drawcolbutt(gctype, butt, str, col) drawbutton(gctype, butt, str, 0, 0, col)
+#define drawoffsbutt(gctype, butt, str, xoffs, yoffs) drawbutton(gctype, butt, str, xoffs, yoffs, DEFAULTKEYCOL)
 
 	// Jump and spin
 	if (drawgamecontrols)
 	{
-		drawbutt(gc_jump,  BT_JUMP, "JMP");
-		drawbutt(gc_use,   BT_USE,  "SPN");
+		drawbutt(gc_jump,  BT_JUMP, NULL);
+		drawbutt(gc_use,   BT_USE,  NULL);
 	}
 
 	// Control panel

xdf22/SRB2

src/g_input.c
@@ -1270,12 +1270,14 @@ static void G_DefineTouchGameControls(void)
 		touchcontrols[GC_STRAFERIGHT].h = 14;
 
 		// Jump
+		touchcontrols[GC_JUMP].name = "JMP";
 		touchcontrols[GC_JUMP].w = 40;
 		touchcontrols[GC_JUMP].h = 32;
 		touchcontrols[GC_JUMP].x = ((vid.width / vid.dup) - touchcontrols[GC_JUMP].w - corneroffset - 12);
 		touchcontrols[GC_JUMP].y = ((vid.height / vid.dup) - touchcontrols[GC_JUMP].h - corneroffset - 12) + offs;
 
 		// Spin
+		touchcontrols[GC_SPIN].name = "SPN";
 		touchcontrols[GC_SPIN].w = 32;
 		touchcontrols[GC_SPIN].h = 24;
 		touchcontrols[GC_SPIN].x = (touchcontrols[GC_JUMP].x - touchcontrols[GC_SPIN].w - 12);
@@ -1323,13 +1325,15 @@ static void G_DefineTouchGameControls(void)
 		touchcontrols[GC_STRAFERIGHT].h = 28;
 
 		// Jump
+		touchcontrols[GC_JUMP].name = "JUMP";
 		touchcontrols[GC_JUMP].w = 48;
 		touchcontrols[GC_JUMP].h = 48;
 		touchcontrols[GC_JUMP].x = ((vid.width / vid.dup) - touchcontrols[GC_JUMP].w - corneroffset - 12);
 		touchcontrols[GC_JUMP].y = ((vid.height / vid.dup) - touchcontrols[GC_JUMP].h - corneroffset - 12) + offs;
 
 		// Spin
-		touchcontrols[GC_SPIN].w = 32;
+		touchcontrols[GC_SPIN].name = "SPIN";
+		touchcontrols[GC_SPIN].w = 40;
 		touchcontrols[GC_SPIN].h = 32;
 		touchcontrols[GC_SPIN].x = (touchcontrols[GC_JUMP].x - touchcontrols[GC_SPIN].w - 12);
 		touchcontrols[GC_SPIN].y = touchcontrols[GC_JUMP].y + 12;

src/st_stuff.c
@@ -1497,9 +1497,10 @@ void ST_drawTouchGameInput(boolean drawgamecontrols)
 	}
 
 #define DEFAULTKEYCOL 16 // Because of macro expansion, this define needs to be up here.
-#define drawbutton(gctype, butt, symb, strxoffs, stryoffs, keycol) { \
+#define drawbutton(gctype, butt, str, strxoffs, stryoffs, keycol) { \
 	touchconfig_t *control = &touchcontrols[gctype]; \
 	if (!control->hidden) { \
+		const char *keystr = ((str == NULL) ? control->name : str); \
 		SCALEBUTTON(control); \
 		if ((butt != 0 && (stplyr->cmd.buttons & butt)) \
 		|| control->pressed > I_GetTime()) \
@@ -1514,21 +1515,21 @@ void ST_drawTouchGameInput(boolean drawgamecontrols)
 			V_DrawFill(x, y + h, w, shadow, 29|flags); \
 		} \
 		V_DrawFill(x, y + offs, w, h, col|flags); \
-		V_DrawString((x + (w / 2)) - (V_StringWidth(symb, flags) / 2) + strxoffs, \
+		V_DrawString((x + (w / 2)) - (V_StringWidth(keystr, flags) / 2) + strxoffs, \
 					((y + (h / 2)) - ((8*vid.dup) / 2) + offs) + stryoffs, \
-					flags, symb); \
+					flags, keystr); \
 		} \
 	}
 
-#define drawbutt(gctype, butt, symb) drawbutton(gctype, butt, symb, 0, 0, DEFAULTKEYCOL)
-#define drawcolbutt(gctype, butt, symb, col) drawbutton(gctype, butt, symb, 0, 0, col)
-#define drawoffsbutt(gctype, butt, symb, xoffs, yoffs) drawbutton(gctype, butt, symb, xoffs, yoffs, DEFAULTKEYCOL)
+#define drawbutt(gctype, butt, str) drawbutton(gctype, butt, str, 0, 0, DEFAULTKEYCOL)
+#define drawcolbutt(gctype, butt, str, col) drawbutton(gctype, butt, str, 0, 0, col)
+#define drawoffsbutt(gctype, butt, str, xoffs, yoffs) drawbutton(gctype, butt, str, xoffs, yoffs, DEFAULTKEYCOL)
 
 	// Jump and spin
 	if (drawgamecontrols)
 	{
-		drawbutt(GC_JUMP,  BT_JUMP, "JMP");
-		drawbutt(GC_SPIN,   BT_SPIN,  "SPN");
+		drawbutt(GC_JUMP,  BT_JUMP, NULL);
+		drawbutt(GC_SPIN,   BT_SPIN,  NULL);
 	}
 
 	// Control panel