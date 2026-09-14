SRB2-Mobile/SRB2-Android

Improve touch input in-game

src/g_input.c
@@ -44,6 +44,7 @@ UINT8 gamekeydown[NUMINPUTS];
 
 // Lactozilla: Touch input buttons
 #ifdef TOUCHINPUTS
+UINT8 touchfingers[NUMTOUCHFINGERS];
 touchconfig_t touchconfig[num_gamecontrols];
 #endif
 
@@ -118,6 +119,9 @@ void G_MapEventsToControls(event_t *ev)
 {
 	INT32 i;
 	UINT8 flag;
+#ifdef TOUCHINPUTS
+	INT32 gc;
+#endif
 
 	switch (ev->type)
 	{
@@ -146,23 +150,42 @@ void G_MapEventsToControls(event_t *ev)
 
 #ifdef TOUCHINPUTS
 		case ev_touchdown:
+		case ev_touchmotion:
+			// Lactozilla: Find every on-screen button and
+			// check if they are below your finger.
+			// ev->data3 is the finger's ID.
 			for (i = 0; i < num_gamecontrols; i++)
 			{
 				INT32 x = ev->data1;
 				INT32 y = ev->data2;
 				touchconfig_t *butt = &touchconfig[i];
+
+				// In a touch motion event, simulate a key up event by clearing gamekeydown.
+				// This is done so that the buttons that are down don't 'stick'
+				// if you move your finger from a button to another.
+				gc = ev->data3; // the finger ID
+				if (ev->type == ev_touchmotion && touchfingers[gc])
+				{
+					// Let go of this button.
+					gamekeydown[touchfingers[ev->data3]] = 0;
+					touchfingers[ev->data3] = 0;
+				}
+
+				// Check if your finger touches this button.
 				if (x >= butt->x && x <= butt->x + butt->w && y >= butt->y && y <= butt->y + butt->h)
 				{
-					gamekeydown[gamecontrol[i][0]] = 1;
+					gc = gamecontrol[i][0];
+					touchfingers[ev->data3] = gc;
+					gamekeydown[gc] = 1;
 					break;
 				}
 			}
 			break;
 
 		case ev_touchup:
-			// lol
-			for (i = 0; i < num_gamecontrols; i++)
-				gamekeydown[gamecontrol[i][0]] = 0;
+			// Let go of this button.
+			gamekeydown[touchfingers[ev->data3]] = 0;
+			touchfingers[ev->data3] = 0;
 			break;
 #endif
 
@@ -201,8 +224,6 @@ void G_MapEventsToControls(event_t *ev)
 		default:
 			break;
 	}
-
-#undef THISMACRONEEDSANAME
 
 	// ALWAYS check for mouse & joystick double-clicks even if no mouse event
 	for (i = 0; i < MOUSEBUTTONS; i++)

src/sdl/i_video.c
@@ -887,6 +887,9 @@ static void Impl_HandleJoystickButtonEvent(SDL_JoyButtonEvent evt, Uint32 type)
 // Lactozilla: Android touch inputs
 static void Impl_HandleTouchEvent(SDL_TouchFingerEvent evt)
 {
+	event_t event;
+	INT32 finger;
+
 	float touchx = evt.x;
 	float touchy = evt.y;
 
@@ -898,47 +901,68 @@ static void Impl_HandleTouchEvent(SDL_TouchFingerEvent evt)
 	if (touchy >= 0.0 && touchy <= 1.0)
 		screeny = touchy * BASEVIDHEIGHT;
 
-#if defined(LOGCAT)
-	if (screenx != -1 && screeny != -1)
-		CONS_Printf("YAYYYY!!!!! x %d y %d %d %d\n", screenx, screeny, evt.fingerId, evt.touchId);
+	finger = (INT32)evt.fingerId;
+	if (finger >= NUMTOUCHFINGERS)
+	{
+		CONS_Alert(CONS_NOTICE, "More than %d fingers not supported, please only use up to two hands or paws\n", NUMTOUCHFINGERS);
+		return;
+	}
 
-	if (evt.type == SDL_FINGERMOTION)
-		CONS_Printf("Finger motion\n");
-	else if (evt.type == SDL_FINGERUP)
-		CONS_Printf("Finger up\n");
-	else if (evt.type == SDL_FINGERDOWN)
-		CONS_Printf("Finger down\n");
+	// outside the screen
+	if (screenx == -1 || screeny == -1)
+		return;
+#if defined(LOGCAT)
+	else
+	{
+		const char *fingertype = "???";
+		if (evt.type == SDL_FINGERMOTION)
+			fingertype = "Finger motion";
+		else if (evt.type == SDL_FINGERUP)
+			fingertype = "Finger up";
+		else if (evt.type == SDL_FINGERDOWN)
+			fingertype = "Finger down";
+		CONS_Printf("%s: x %d y %d fingerid %d touchid %d\n", fingertype, screenx, screeny, finger, (int)evt.touchId);
+	}
 #endif
 
-	if (evt.type == SDL_FINGERDOWN || evt.type == SDL_FINGERUP)
+	switch (gamestate)
 	{
-		event_t event;
-		switch (gamestate)
-		{
-			case GS_LEVEL:
-				if (menuactive || promptactive)
-				{
-					event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
-					event.data1 = KEY_SPACE;
-				}
-				else
-				{
-					event.type = (evt.type == SDL_FINGERDOWN) ? ev_touchdown : ev_touchup;
-					event.data1 = screenx;
-					event.data2 = screeny;
-				}
-				break;
-			default:
+		case GS_LEVEL:
+			if (menuactive || promptactive)
+			{
 				event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
-				event.data1 = KEY_ENTER;
-				break;
-		}
-		D_PostEvent(&event);
+				event.data1 = KEY_SPACE;
+			}
+			else
+			{
+				switch (evt.type)
+				{
+					case SDL_FINGERMOTION:
+						event.type = ev_touchmotion;
+						break;
+					case SDL_FINGERDOWN:
+						event.type = ev_touchdown;
+						break;
+					case SDL_FINGERUP:
+						event.type = ev_touchup;
+						break;
+					default:
+						// Don't generate any event.
+						return;
+				}
+				event.data1 = screenx;
+				event.data2 = screeny;
+				event.data3 = finger;
+			}
+			break;
+		default:
+			event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
+			event.data1 = KEY_ENTER;
+			break;
 	}
+	D_PostEvent(&event);
 }
 #endif
-
-
 
 void I_GetEvent(void)
 {

xdf22/SRB2

src/g_input.c
@@ -44,6 +44,7 @@ UINT8 gamekeydown[NUMINPUTS];
 
 // Lactozilla: Touch input buttons
 #ifdef TOUCHINPUTS
+UINT8 touchfingers[NUMTOUCHFINGERS];
 touchconfig_t touchconfig[NUM_GAMECONTROLS];
 #endif
 
@@ -118,6 +119,9 @@ void G_MapEventsToControls(event_t *ev)
 {
 	INT32 i;
 	UINT8 flag;
+#ifdef TOUCHINPUTS
+	INT32 gc;
+#endif
 
 	switch (ev->type)
 	{
@@ -149,23 +153,42 @@ void G_MapEventsToControls(event_t *ev)
 
 #ifdef TOUCHINPUTS
 		case ev_touchdown:
+		case ev_touchmotion:
+			// Lactozilla: Find every on-screen button and
+			// check if they are below your finger.
+			// ev->data3 is the finger's ID.
 			for (i = 0; i < NUM_GAMECONTROLS; i++)
 			{
 				INT32 x = ev->x;
 				INT32 y = ev->y;
 				touchconfig_t *butt = &touchconfig[i];
+
+				// In a touch motion event, simulate a key up event by clearing gamekeydown.
+				// This is done so that the buttons that are down don't 'stick'
+				// if you move your finger from a button to another.
+				gc = ev->y; // the finger ID
+				if (ev->type == ev_touchmotion && touchfingers[gc])
+				{
+					// Let go of this button.
+					gamekeydown[touchfingers[ev->y]] = 0;
+					touchfingers[ev->y] = 0;
+				}
+
+				// Check if your finger touches this button.
 				if (x >= butt->x && x <= butt->x + butt->w && y >= butt->y && y <= butt->y + butt->h)
 				{
-					gamekeydown[gamecontrol[i][0]] = 1;
+					gc = gamecontrol[i][0];
+					touchfingers[ev->y] = gc;
+					gamekeydown[gc] = 1;
 					break;
 				}
 			}
 			break;
 
 		case ev_touchup:
-			// lol
-			for (i = 0; i < NUM_GAMECONTROLS; i++)
-				gamekeydown[gamecontrol[i][0]] = 0;
+			// Let go of this button.
+			gamekeydown[touchfingers[ev->y]] = 0;
+			touchfingers[ev->y] = 0;
 			break;
 #endif
 
@@ -200,8 +223,6 @@ void G_MapEventsToControls(event_t *ev)
 		default:
 			break;
 	}
-
-#undef THISMACRONEEDSANAME
 
 	// ALWAYS check for mouse & joystick double-clicks even if no mouse event
 	for (i = 0; i < MOUSEBUTTONS; i++)

src/sdl/i_video.c
@@ -978,6 +978,9 @@ static void Impl_HandleJoystickButtonEvent(SDL_JoyButtonEvent evt, Uint32 type)
 // Lactozilla: Android touch inputs
 static void Impl_HandleTouchEvent(SDL_TouchFingerEvent evt)
 {
+	event_t event;
+	INT32 finger;
+
 	float touchx = evt.x;
 	float touchy = evt.y;
 
@@ -989,47 +992,68 @@ static void Impl_HandleTouchEvent(SDL_TouchFingerEvent evt)
 	if (touchy >= 0.0 && touchy <= 1.0)
 		screeny = touchy * BASEVIDHEIGHT;
 
-#if defined(LOGCAT)
-	if (screenx != -1 && screeny != -1)
-		CONS_Printf("YAYYYY!!!!! x %d y %d %d %d\n", screenx, screeny, evt.fingerId, evt.touchId);
+	finger = (INT32)evt.fingerId;
+	if (finger >= NUMTOUCHFINGERS)
+	{
+		CONS_Alert(CONS_NOTICE, "More than %d fingers not supported, please only use up to two hands or paws\n", NUMTOUCHFINGERS);
+		finger = 1; // temp
+	}
 
-	if (evt.type == SDL_FINGERMOTION)
-		CONS_Printf("Finger motion\n");
-	else if (evt.type == SDL_FINGERUP)
-		CONS_Printf("Finger up\n");
-	else if (evt.type == SDL_FINGERDOWN)
-		CONS_Printf("Finger down\n");
+	// outside the screen
+	if (screenx == -1 || screeny == -1)
+		return;
+#if defined(LOGCAT)
+	else
+	{
+		const char *fingertype = "???";
+		if (evt.type == SDL_FINGERMOTION)
+			fingertype = "Finger motion";
+		else if (evt.type == SDL_FINGERUP)
+			fingertype = "Finger up";
+		else if (evt.type == SDL_FINGERDOWN)
+			fingertype = "Finger down";
+		CONS_Printf("%s: x %d y %d fingerid %d touchid %d\n", fingertype, screenx, screeny, finger, (int)evt.touchId);
+	}
 #endif
 
-	if (evt.type == SDL_FINGERDOWN || evt.type == SDL_FINGERUP)
+	switch (gamestate)
 	{
-		event_t event;
-		switch (gamestate)
-		{
-			case GS_LEVEL:
-				if (menuactive || promptactive)
-				{
-					event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
-					event.key = KEY_SPACE;
-				}
-				else
-				{
-					event.type = (evt.type == SDL_FINGERDOWN) ? ev_touchdown : ev_touchup;
-					event.x = screenx;
-					event.y = screeny;
-				}
-				break;
-			default:
+		case GS_LEVEL:
+			if (menuactive || promptactive)
+			{
 				event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
-				event.key = KEY_ENTER;
-				break;
-		}
-		D_PostEvent(&event);
+				event.key = KEY_SPACE;
+			}
+			else
+			{
+				switch (evt.type)
+				{
+					case SDL_FINGERMOTION:
+						event.type = ev_touchmotion;
+						break;
+					case SDL_FINGERDOWN:
+						event.type = ev_touchdown;
+						break;
+					case SDL_FINGERUP:
+						event.type = ev_touchup;
+						break;
+					default:
+						// Don't generate any event.
+						return;
+				}
+				event.x = screenx;
+				event.y = screeny;
+				event.repeated = finger;
+			}
+			break;
+		default:
+			event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
+			event.key = KEY_ENTER;
+			break;
 	}
+	D_PostEvent(&event);
 }
 #endif
-
-
 
 void I_GetEvent(void)
 {