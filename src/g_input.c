SRB2-Mobile/SRB2-Android

Add touch input options to the menu

src/g_input.c
@@ -904,7 +904,7 @@ static void G_DefineTouchGameControls(void)
 	offs += bottomalign;
 
 	// clear all
-	memset(touchcontrols, 0x00, sizeof(num_gamecontrols) * NUMKEYS);
+	memset(touchcontrols, 0x00, sizeof(touchconfig_t) * num_gamecontrols);
 
 	if (touch_dpad_tiny)
 	{
@@ -985,7 +985,7 @@ static void G_DefineTouchGameControls(void)
 		touchcontrols[gc_straferight].h = 28;
 
 		// Spin
-		touchcontrols[gc_use].x = 232 + rightalign;
+		touchcontrols[gc_use].x = (232 + rightalign) - 12;
 		touchcontrols[gc_use].y = 148 + offs;
 		touchcontrols[gc_use].w = 32;
 		touchcontrols[gc_use].h = 32;

xdf22/SRB2

src/g_input.c
@@ -910,7 +910,7 @@ static void G_DefineTouchGameControls(void)
 	offs += bottomalign;
 
 	// clear all
-	memset(touchcontrols, 0x00, sizeof(num_gamecontrols) * NUMKEYS);
+	memset(touchcontrols, 0x00, sizeof(touchconfig_t) * NUM_GAMECONTROLS);
 
 	if (touch_dpad_tiny)
 	{
@@ -991,7 +991,7 @@ static void G_DefineTouchGameControls(void)
 		touchcontrols[GC_STRAFERIGHT].h = 28;
 
 		// Spin
-		touchcontrols[GC_SPIN].x = 232 + rightalign;
+		touchcontrols[GC_SPIN].x = (232 + rightalign) - 12;
 		touchcontrols[GC_SPIN].y = 148 + offs;
 		touchcontrols[GC_SPIN].w = 32;
 		touchcontrols[GC_SPIN].h = 32;
@@ -1068,7 +1068,7 @@ static void G_DefineTouchNavigation(void)
 	}
 	else
 	{
-		touchnavigation[KEY_ENTER].x = ((vid.width / vid.dupx) - touchnavigation[KEY_ENTER].w - left);
+		touchnavigation[KEY_ENTER].x = ((vid.width / vid.dup) - touchnavigation[KEY_ENTER].w - left);
 		touchnavigation[KEY_ENTER].y = touchnavigation[KEY_ESCAPE].y;
 	}
 }