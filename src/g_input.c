SRB2-Mobile/SRB2-Android

Touch input test WIP

src/g_input.c
@@ -42,6 +42,11 @@ INT32 joyxmove[JOYAXISSET], joyymove[JOYAXISSET], joy2xmove[JOYAXISSET], joy2ymo
 // current state of the keys: true if pushed
 UINT8 gamekeydown[NUMINPUTS];
 
+// Lactozilla: Touch input buttons
+#ifdef TOUCHINPUTS
+touchconfig_t touchconfig[num_gamecontrols];
+#endif
+
 // two key codes (or virtual key) per game control
 INT32 gamecontrol[num_gamecontrols][2];
 INT32 gamecontrolbis[num_gamecontrols][2]; // secondary splitscreen player
@@ -139,6 +144,28 @@ void G_MapEventsToControls(event_t *ev)
 #endif
 			break;
 
+#ifdef TOUCHINPUTS
+		case ev_touchdown:
+			for (i = 0; i < num_gamecontrols; i++)
+			{
+				INT32 x = ev->data1;
+				INT32 y = ev->data2;
+				touchconfig_t *butt = &touchconfig[i];
+				if (x >= butt->x && x <= butt->x + butt->w && y >= butt->y && y <= butt->y + butt->h)
+				{
+					gamekeydown[gamecontrol[i][0]] = 1;
+					break;
+				}
+			}
+			break;
+
+		case ev_touchup:
+			// lol
+			for (i = 0; i < num_gamecontrols; i++)
+				gamekeydown[gamecontrol[i][0]] = 0;
+			break;
+#endif
+
 		case ev_mouse: // buttons are virtual keys
 			if (menuactive || CON_Ready() || chat_on)
 				break;
@@ -174,6 +201,8 @@ void G_MapEventsToControls(event_t *ev)
 		default:
 			break;
 	}
+
+#undef THISMACRONEEDSANAME
 
 	// ALWAYS check for mouse & joystick double-clicks even if no mouse event
 	for (i = 0; i < MOUSEBUTTONS; i++)
@@ -769,7 +798,112 @@ void G_DefineDefaultControls(void)
 		//gamecontrolbisdefault[i][gc_talkkey   ][0] = KEY_2HAT1+2; // D-Pad Left
 		//gamecontrolbisdefault[i][gc_scores    ][0] = KEY_2HAT1+3; // D-Pad Right
 	}
+
+#ifdef TOUCHINPUTS
+	touch_dpad_tiny = true;
+	G_DefineTouchControls();
+#endif
 }
+
+// Lactozilla: Touch input buttons
+#ifdef TOUCHINPUTS
+INT32 touch_dpad_x, touch_dpad_y, touch_dpad_w, touch_dpad_h;
+boolean touch_dpad_tiny;
+
+void G_DefineTouchControls(void)
+{
+	if (touch_dpad_tiny)
+	{
+		touch_dpad_x = 24;
+		touch_dpad_y = 128;
+		touch_dpad_w = 32;
+		touch_dpad_h = 32;
+
+		// Up
+		touchconfig[gc_forward].x = touch_dpad_x + 8;
+		touchconfig[gc_forward].y = touch_dpad_y - 8;
+		touchconfig[gc_forward].w = 20;
+		touchconfig[gc_forward].h = 16;
+
+		// Down
+		touchconfig[gc_backward].x = touch_dpad_x + 8;
+		touchconfig[gc_backward].y = touch_dpad_y + 24;
+		touchconfig[gc_backward].w = 20;
+		touchconfig[gc_backward].h = 16;
+
+		// Left
+		touchconfig[gc_strafeleft].x = touch_dpad_x - 8;
+		touchconfig[gc_strafeleft].y = touch_dpad_y + 8;
+		touchconfig[gc_strafeleft].w = 16;
+		touchconfig[gc_strafeleft].h = 14;
+
+		// Right
+		touchconfig[gc_straferight].x = touch_dpad_x + 24;
+		touchconfig[gc_straferight].y = touch_dpad_y + 8;
+		touchconfig[gc_straferight].w = 16;
+		touchconfig[gc_straferight].h = 14;
+
+		// Spin
+		touchconfig[gc_use].x = 232;
+		touchconfig[gc_use].y = 148;
+		touchconfig[gc_use].w = 24;
+		touchconfig[gc_use].h = 24;
+
+		// Jump
+		touchconfig[gc_jump].x = touchconfig[gc_use].x + touchconfig[gc_use].w + 12;
+		touchconfig[gc_jump].y = touchconfig[gc_use].y;
+		touchconfig[gc_jump].w = 24;
+		touchconfig[gc_jump].h = 24;
+	}
+	else
+	{
+		INT32 x;
+
+		touch_dpad_x = 24;
+		touch_dpad_y = 92;
+		touch_dpad_w = 64;
+		touch_dpad_h = 64;
+
+		x = (touch_dpad_x + touch_dpad_w) - (touch_dpad_w / 2);
+
+		// Up
+		touchconfig[gc_forward].x = x - 12;
+		touchconfig[gc_forward].y = touch_dpad_y - (touch_dpad_w / 4);
+		touchconfig[gc_forward].w = 40;
+		touchconfig[gc_forward].h = 32;
+
+		// Down
+		touchconfig[gc_backward].x = x - 12;
+		touchconfig[gc_backward].y = (touch_dpad_y + touch_dpad_h) - (touch_dpad_w / 4);
+		touchconfig[gc_backward].w = 40;
+		touchconfig[gc_backward].h = 32;
+
+		// Left
+		touchconfig[gc_strafeleft].x = touch_dpad_x - (touch_dpad_w / 4);
+		touchconfig[gc_strafeleft].y = touch_dpad_y + (touch_dpad_w / 4);
+		touchconfig[gc_strafeleft].w = 32;
+		touchconfig[gc_strafeleft].h = 28;
+
+		// Right
+		touchconfig[gc_straferight].x = (touch_dpad_x + touch_dpad_w) - (touch_dpad_w / 4);
+		touchconfig[gc_straferight].y = touch_dpad_y + (touch_dpad_w / 4);
+		touchconfig[gc_straferight].w = 32;
+		touchconfig[gc_straferight].h = 28;
+
+		// Spin
+		touchconfig[gc_use].x = 232;
+		touchconfig[gc_use].y = 148;
+		touchconfig[gc_use].w = 32;
+		touchconfig[gc_use].h = 32;
+
+		// Jump
+		touchconfig[gc_jump].x = touchconfig[gc_use].x + touchconfig[gc_use].w + 16;
+		touchconfig[gc_jump].y = touchconfig[gc_use].y;
+		touchconfig[gc_jump].w = 32;
+		touchconfig[gc_jump].h = 32;
+	}
+}
+#endif
 
 INT32 G_GetControlScheme(INT32 (*fromcontrols)[2], const INT32 *gclist, INT32 gclen)
 {

src/m_menu.c
@@ -8736,12 +8736,14 @@ static void M_LoadGame(INT32 choice)
 {
 	(void)choice;
 
+#if !defined(__ANDROID__)
 	if (tutorialmap && cv_tutorialprompt.value)
 	{
 		M_StartMessage("Do you want to \x82play a brief Tutorial\x80?\n\nWe highly recommend this because \nthe controls are slightly different \nfrom other games.\n\nPress 'Y' or 'Enter' to go\nPress 'N' or any key to skip\n",
 			M_FirstTimeResponse, MM_YESNO);
 		return;
 	}
+#endif
 
 	M_ReadSaveStrings();
 	M_SetupNextMenu(&SP_LoadDef);

src/sdl/i_video.c
@@ -69,6 +69,7 @@
 #include "../st_stuff.h"
 #include "../hu_stuff.h"
 #include "../g_game.h"
+#include "../g_input.h"
 #include "../i_video.h"
 #include "../console.h"
 #include "../command.h"
@@ -356,6 +357,11 @@ static INT32 Impl_SDL_Scancode_To_Keycode(SDL_Scancode code)
 		case SDL_SCANCODE_RALT:   return KEY_RALT;
 		case SDL_SCANCODE_LGUI:   return KEY_LEFTWIN;
 		case SDL_SCANCODE_RGUI:   return KEY_RIGHTWIN;
+
+#if defined(__ANDROID__)
+		case SDL_SCANCODE_AC_BACK:        return KEY_ESCAPE;
+#endif
+
 		default:                  break;
 	}
 #ifdef HWRENDER
@@ -877,6 +883,61 @@ static void Impl_HandleJoystickButtonEvent(SDL_JoyButtonEvent evt, Uint32 type)
 	if (event.type != ev_console) D_PostEvent(&event);
 }
 
+#ifdef TOUCHINPUTS
+// Lactozilla: Android touch inputs
+static void Impl_HandleTouchEvent(SDL_TouchFingerEvent evt)
+{
+	float touchx = evt.x;
+	float touchy = evt.y;
+
+	INT32 screenx = -1;
+	INT32 screeny = -1;
+
+	if (touchx >= 0.0 && touchx <= 1.0)
+		screenx = touchx * BASEVIDWIDTH;
+	if (touchy >= 0.0 && touchy <= 1.0)
+		screeny = touchy * BASEVIDHEIGHT;
+
+#if defined(LOGCAT)
+	if (screenx != -1 && screeny != -1)
+		CONS_Printf("YAYYYY!!!!! x %d y %d %d %d\n", screenx, screeny, evt.fingerId, evt.touchId);
+
+	if (evt.type == SDL_FINGERMOTION)
+		CONS_Printf("Finger motion\n");
+	else if (evt.type == SDL_FINGERUP)
+		CONS_Printf("Finger up\n");
+	else if (evt.type == SDL_FINGERDOWN)
+		CONS_Printf("Finger down\n");
+#endif
+
+	if (evt.type == SDL_FINGERDOWN || evt.type == SDL_FINGERUP)
+	{
+		event_t event;
+		switch (gamestate)
+		{
+			case GS_LEVEL:
+				if (menuactive || promptactive)
+				{
+					event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
+					event.data1 = KEY_SPACE;
+				}
+				else
+				{
+					event.type = (evt.type == SDL_FINGERDOWN) ? ev_touchdown : ev_touchup;
+					event.data1 = screenx;
+					event.data2 = screeny;
+				}
+				break;
+			default:
+				event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
+				event.data1 = KEY_ENTER;
+				break;
+		}
+		D_PostEvent(&event);
+	}
+}
+#endif
+
 
 
 void I_GetEvent(void)
@@ -919,6 +980,13 @@ void I_GetEvent(void)
 			case SDL_JOYAXISMOTION:
 				Impl_HandleJoystickAxisEvent(evt.jaxis);
 				break;
+#ifdef TOUCHINPUTS
+			case SDL_FINGERMOTION:
+			case SDL_FINGERDOWN:
+			case SDL_FINGERUP:
+				Impl_HandleTouchEvent(evt.tfinger);
+				break;
+#endif
 #if 0
 			case SDL_JOYHATMOTION:
 				Impl_HandleJoystickHatEvent(evt.jhat)

src/st_stuff.c
@@ -15,6 +15,7 @@
 
 #include "doomdef.h"
 #include "g_game.h"
+#include "g_input.h"
 #include "r_local.h"
 #include "p_local.h"
 #include "f_finale.h"
@@ -1201,6 +1202,211 @@ static void ST_drawInput(void)
 		V_DrawThinString(x, y, hudinfo[HUD_LIVES].f|((leveltime & 4) ? V_YELLOWMAP : V_REDMAP), "BAD DEMO!!");
 }
 
+#ifdef TOUCHINPUTS
+static void ST_drawTouchInput(void)
+{
+	fixed_t dupx = vid.dupx*FRACUNIT; //FixedDiv(vid.dupx*FRACUNIT, FRACUNIT*2);
+	fixed_t dupy = vid.dupy*FRACUNIT; //FixedDiv(vid.dupy*FRACUNIT, FRACUNIT*2);
+	const INT32 flags = V_NOSCALESTART;
+	const INT32 accent = (stplyr->skincolor ? Color_Index[stplyr->skincolor-1][4] : 0);
+	const INT32 shadow = vid.dupy;
+	INT32 col, offs;
+	INT32 base, ybase;
+	INT32 xslant, yslant;
+	INT32 udw;
+	INT32 i, j;
+
+	INT32 x = FixedMul(touch_dpad_x * FRACUNIT, dupx) / FRACUNIT;
+	INT32 y = FixedMul(touch_dpad_y * FRACUNIT, dupy) / FRACUNIT;
+	INT32 w = FixedMul(touch_dpad_w * FRACUNIT, dupx) / FRACUNIT;
+	INT32 h = FixedMul(touch_dpad_h * FRACUNIT, dupy) / FRACUNIT;
+
+	touchconfig_t *tleft = &touchconfig[gc_strafeleft];
+	touchconfig_t *tright = &touchconfig[gc_straferight];
+	touchconfig_t *tup = &touchconfig[gc_forward];
+	touchconfig_t *tdown = &touchconfig[gc_backward];
+
+	touchconfig_t *tjump = &touchconfig[gc_jump];
+	touchconfig_t *tspin = &touchconfig[gc_use];
+
+	if (stplyr->powers[pw_carry] == CR_NIGHTSMODE)
+		y -= FixedMul(16 * FRACUNIT, dupy) / FRACUNIT;
+
+	if (F_GetPromptHideHud(y))
+		return;
+
+#define CENTERPOS \
+	if (vid.height != BASEVIDHEIGHT * vid.dupy) \
+		y += (vid.height - (BASEVIDHEIGHT * vid.dupy)); \
+
+	// O backing
+	CENTERPOS;
+	V_DrawFill(x, y-1, w, h, flags|20);
+	V_DrawFill(x, y+h-1, w, shadow, flags|29);
+
+	if (vid.dupx == 1)
+		udw = 2;
+	else
+		udw = (vid.dupx * 3);
+
+#define THISMACRONEEDSANAME(touch) \
+	x = FixedMul(touch->x * FRACUNIT, dupx) / FRACUNIT; \
+	y = FixedMul(touch->y * FRACUNIT, dupy) / FRACUNIT; \
+	w = FixedMul(touch->w * FRACUNIT, dupx) / FRACUNIT; \
+	h = FixedMul(touch->h * FRACUNIT, dupy) / FRACUNIT; \
+	xslant = FixedMul((touch->w/2) * FRACUNIT, dupx) / FRACUNIT; \
+	yslant = FixedMul((touch->h/2) * FRACUNIT, dupy) / FRACUNIT; \
+	CENTERPOS;
+
+	// <
+	THISMACRONEEDSANAME(tleft);
+
+	base = (w - xslant);
+	ybase = (y + h) - vid.dupy;
+
+#define drawleftbutton(color, offset) \
+	V_DrawFill(x, y+offset, base+(vid.dupx), h, color|flags); \
+	for (i = 0; i < xslant; i++) \
+		V_DrawFill(x+base+i+(vid.dupx), (y+i)+offset, vid.dupx, h-(i*2), color|flags);
+
+	if (stplyr->cmd.sidemove < 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawleftbutton(29, shadow);
+	}
+
+	drawleftbutton(col, offs);
+
+	// ^
+	THISMACRONEEDSANAME(tup);
+
+	yslant /= 2;
+	yslant += (vid.dupy * 2) + 1;
+
+	base = w;
+	ybase = (y + h) - vid.dupy;
+
+#define drawupbutton(color, offset) \
+	for (i = 0; i < yslant; i++) \
+		V_DrawFill(x+i, y+offset, 1, (h-yslant)+i, color|flags); \
+	ybase = (h-yslant)+i; \
+	V_DrawFill(x+i, y+offset, udw, ybase, color|flags); \
+	for (j = 0; j < yslant; j++) \
+		V_DrawFill(x+i+j+udw, y+offset, 1, (ybase-(j+1)), color|flags); \
+
+	if (stplyr->cmd.forwardmove > 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawupbutton(29, shadow);
+	}
+
+	drawupbutton(col, offs);
+
+	// >
+	THISMACRONEEDSANAME(tright);
+
+	base = (w - xslant);
+	ybase = (y + h) - vid.dupy;
+
+#define drawrightbutton(color, offset) \
+	V_DrawFill(x+base-(vid.dupx), y+offset, base+(vid.dupx), h, color|flags); \
+	for (i = 0; i < xslant; i++) \
+		V_DrawFill(x+(base-(vid.dupx))-i-(vid.dupx), (y+i)+offset, vid.dupx, h-(i*2), color|flags);
+
+	if (stplyr->cmd.sidemove > 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawrightbutton(29, shadow);
+	}
+
+	drawrightbutton(col, offs);
+
+	// v
+	THISMACRONEEDSANAME(tdown);
+
+	yslant /= 2;
+	yslant += (vid.dupy * 2) + 1;
+
+	base = w;
+	ybase = (y + h);
+
+#define drawdownbutton(color, offset) \
+	for (i = 0; i < yslant; i++) \
+		V_DrawFill(x+i, (y+(yslant-i)) + offset, 1, (h-yslant)+i, color|flags); \
+	ybase = (h-yslant)+i; \
+	V_DrawFill(x+i, y+offset, udw, ybase, color|flags); \
+	for (j = 0; j < yslant; j++) \
+		V_DrawFill(x+i+j+udw, ((y+(yslant-i))+j) + 1 + offset, 1, (ybase-(j+1)), color|flags);
+
+	if (stplyr->cmd.forwardmove < 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawdownbutton(29, shadow);
+	}
+
+	drawdownbutton(col, offs);
+
+#undef CENTERPOS
+#define CENTERPOS \
+	if (vid.width != BASEVIDWIDTH * vid.dupx) \
+		x += (vid.width - (BASEVIDWIDTH * vid.dupx)); \
+	if (vid.height != BASEVIDHEIGHT * vid.dupy) \
+		y += (vid.height - (BASEVIDHEIGHT * vid.dupy)); \
+
+#define drawbutt(control, butt, symb) \
+	THISMACRONEEDSANAME(control); \
+	if (stplyr->cmd.buttons & butt) \
+	{ \
+		col = accent; \
+		offs = shadow; \
+	} \
+	else \
+	{ \
+		col = 16; \
+		offs = 0; \
+		V_DrawFill(x, y + h, w, shadow, 29|flags); \
+	} \
+	V_DrawFill(x, y + offs, w, h, col|flags); \
+	V_DrawCharacter((x + (w / 2)) - ((8*vid.dupx) / 2), (y + (h / 2)) - ((8*vid.dupx) / 2) + offs, symb|flags, false);
+
+	drawbutt(tjump, BT_JUMP, 'J');
+	drawbutt(tspin, BT_USE,  'S');
+
+#undef drawbutt
+#undef drawdownbutton
+#undef drawrightbutton
+#undef drawupbutton
+#undef drawleftbutton
+#undef THISMACRONEEDSANAME
+#undef CENTERPOS
+
+}
+#endif
+
 static patch_t *lt_patches[3];
 static INT32 lt_scroll = 0;
 static INT32 lt_mom = 0;
@@ -1444,7 +1650,7 @@ static void ST_drawPowerupHUD(void)
 
 	if (stplyr->spectator || stplyr->playerstate != PST_LIVE)
 		return;
-	
+
 // ---------
 // Finish icon
 // ---------
@@ -2816,6 +3022,10 @@ static void ST_overlayDrawer(void)
 
 	if (modeattacking && !(demoplayback && hu_showscores))
 		ST_drawInput();
+#ifdef TOUCHINPUTS
+	else
+		ST_drawTouchInput();
+#endif
 
 	ST_drawDebugInfo();
 }

xdf22/SRB2

src/g_input.c
@@ -42,6 +42,11 @@ INT32 joyxmove[JOYAXISSET], joyymove[JOYAXISSET], joy2xmove[JOYAXISSET], joy2ymo
 // current state of the keys: true if pushed
 UINT8 gamekeydown[NUMINPUTS];
 
+// Lactozilla: Touch input buttons
+#ifdef TOUCHINPUTS
+touchconfig_t touchconfig[NUM_GAMECONTROLS];
+#endif
+
 // two key codes (or virtual key) per game control
 INT32 gamecontrol[NUM_GAMECONTROLS][2];
 INT32 gamecontrolbis[NUM_GAMECONTROLS][2]; // secondary splitscreen player
@@ -142,6 +147,28 @@ void G_MapEventsToControls(event_t *ev)
 #endif
 			break;
 
+#ifdef TOUCHINPUTS
+		case ev_touchdown:
+			for (i = 0; i < NUM_GAMECONTROLS; i++)
+			{
+				INT32 x = ev->x;
+				INT32 y = ev->y;
+				touchconfig_t *butt = &touchconfig[i];
+				if (x >= butt->x && x <= butt->x + butt->w && y >= butt->y && y <= butt->y + butt->h)
+				{
+					gamekeydown[gamecontrol[i][0]] = 1;
+					break;
+				}
+			}
+			break;
+
+		case ev_touchup:
+			// lol
+			for (i = 0; i < NUM_GAMECONTROLS; i++)
+				gamekeydown[gamecontrol[i][0]] = 0;
+			break;
+#endif
+
 		case ev_mouse: // buttons are virtual keys
 			mouse.rdx = ev->x;
 			mouse.rdy = ev->y;
@@ -173,6 +200,8 @@ void G_MapEventsToControls(event_t *ev)
 		default:
 			break;
 	}
+
+#undef THISMACRONEEDSANAME
 
 	// ALWAYS check for mouse & joystick double-clicks even if no mouse event
 	for (i = 0; i < MOUSEBUTTONS; i++)
@@ -775,7 +804,112 @@ void G_DefineDefaultControls(void)
 		gamecontrolbisdefault[i][GC_TOSSFLAG     ][1] = KEY_2HAT1+0; // D-Pad Up
 		//gamecontrolbisdefault[i][GC_SCORES       ][1] = KEY_2HAT1+1; // D-Pad Down
 	}
+
+#ifdef TOUCHINPUTS
+	touch_dpad_tiny = true;
+	G_DefineTouchControls();
+#endif
 }
+
+// Lactozilla: Touch input buttons
+#ifdef TOUCHINPUTS
+INT32 touch_dpad_x, touch_dpad_y, touch_dpad_w, touch_dpad_h;
+boolean touch_dpad_tiny;
+
+void G_DefineTouchControls(void)
+{
+	if (touch_dpad_tiny)
+	{
+		touch_dpad_x = 24;
+		touch_dpad_y = 128;
+		touch_dpad_w = 32;
+		touch_dpad_h = 32;
+
+		// Up
+		touchconfig[GC_FORWARD].x = touch_dpad_x + 8;
+		touchconfig[GC_FORWARD].y = touch_dpad_y - 8;
+		touchconfig[GC_FORWARD].w = 20;
+		touchconfig[GC_FORWARD].h = 16;
+
+		// Down
+		touchconfig[GC_BACKWARD].x = touch_dpad_x + 8;
+		touchconfig[GC_BACKWARD].y = touch_dpad_y + 24;
+		touchconfig[GC_BACKWARD].w = 20;
+		touchconfig[GC_BACKWARD].h = 16;
+
+		// Left
+		touchconfig[GC_STRAFELEFT].x = touch_dpad_x - 8;
+		touchconfig[GC_STRAFELEFT].y = touch_dpad_y + 8;
+		touchconfig[GC_STRAFELEFT].w = 16;
+		touchconfig[GC_STRAFELEFT].h = 14;
+
+		// Right
+		touchconfig[GC_STRAFERIGHT].x = touch_dpad_x + 24;
+		touchconfig[GC_STRAFERIGHT].y = touch_dpad_y + 8;
+		touchconfig[GC_STRAFERIGHT].w = 16;
+		touchconfig[GC_STRAFERIGHT].h = 14;
+
+		// Spin
+		touchconfig[GC_SPIN].x = 232;
+		touchconfig[GC_SPIN].y = 148;
+		touchconfig[GC_SPIN].w = 24;
+		touchconfig[GC_SPIN].h = 24;
+
+		// Jump
+		touchconfig[GC_JUMP].x = touchconfig[GC_SPIN].x + touchconfig[GC_SPIN].w + 12;
+		touchconfig[GC_JUMP].y = touchconfig[GC_SPIN].y;
+		touchconfig[GC_JUMP].w = 24;
+		touchconfig[GC_JUMP].h = 24;
+	}
+	else
+	{
+		INT32 x;
+
+		touch_dpad_x = 24;
+		touch_dpad_y = 92;
+		touch_dpad_w = 64;
+		touch_dpad_h = 64;
+
+		x = (touch_dpad_x + touch_dpad_w) - (touch_dpad_w / 2);
+
+		// Up
+		touchconfig[GC_FORWARD].x = x - 12;
+		touchconfig[GC_FORWARD].y = touch_dpad_y - (touch_dpad_w / 4);
+		touchconfig[GC_FORWARD].w = 40;
+		touchconfig[GC_FORWARD].h = 32;
+
+		// Down
+		touchconfig[GC_BACKWARD].x = x - 12;
+		touchconfig[GC_BACKWARD].y = (touch_dpad_y + touch_dpad_h) - (touch_dpad_w / 4);
+		touchconfig[GC_BACKWARD].w = 40;
+		touchconfig[GC_BACKWARD].h = 32;
+
+		// Left
+		touchconfig[GC_STRAFELEFT].x = touch_dpad_x - (touch_dpad_w / 4);
+		touchconfig[GC_STRAFELEFT].y = touch_dpad_y + (touch_dpad_w / 4);
+		touchconfig[GC_STRAFELEFT].w = 32;
+		touchconfig[GC_STRAFELEFT].h = 28;
+
+		// Right
+		touchconfig[GC_STRAFERIGHT].x = (touch_dpad_x + touch_dpad_w) - (touch_dpad_w / 4);
+		touchconfig[GC_STRAFERIGHT].y = touch_dpad_y + (touch_dpad_w / 4);
+		touchconfig[GC_STRAFERIGHT].w = 32;
+		touchconfig[GC_STRAFERIGHT].h = 28;
+
+		// Spin
+		touchconfig[GC_SPIN].x = 232;
+		touchconfig[GC_SPIN].y = 148;
+		touchconfig[GC_SPIN].w = 32;
+		touchconfig[GC_SPIN].h = 32;
+
+		// Jump
+		touchconfig[GC_JUMP].x = touchconfig[GC_SPIN].x + touchconfig[GC_SPIN].w + 16;
+		touchconfig[GC_JUMP].y = touchconfig[GC_SPIN].y;
+		touchconfig[GC_JUMP].w = 32;
+		touchconfig[GC_JUMP].h = 32;
+	}
+}
+#endif
 
 INT32 G_GetControlScheme(INT32 (*fromcontrols)[2], const INT32 *gclist, INT32 gclen)
 {

src/m_menu.c
@@ -9045,12 +9045,14 @@ static void M_LoadGame(INT32 choice)
 {
 	(void)choice;
 
+#if !defined(IOS)
 	if (tutorialmap && cv_tutorialprompt.value)
 	{
 		M_StartMessage("Do you want to \x82play a brief Tutorial\x80?\n\nWe highly recommend this because \nthe controls are slightly different \nfrom other games.\n\nPress the\x82 Y\x80 key or the\x83 A button\x80 to go\nPress the\x82 N\x80 key or the\x83 Y button\x80 to skip\n",
 			M_FirstTimeResponse, MM_YESNO);
 		return;
 	}
+#endif
 
 	M_ReadSaveStrings();
 	M_SetupNextMenu(&SP_LoadDef);

src/sdl/i_video.c
@@ -74,6 +74,7 @@
 #include "../st_stuff.h"
 #include "../hu_stuff.h"
 #include "../g_game.h"
+#include "../g_input.h"
 #include "../i_video.h"
 #include "../console.h"
 #include "../command.h"
@@ -381,6 +382,11 @@ static INT32 Impl_SDL_Scancode_To_Keycode(SDL_Scancode code)
 		case SDL_SCANCODE_RALT:   return KEY_RALT;
 		case SDL_SCANCODE_LGUI:   return KEY_LEFTWIN;
 		case SDL_SCANCODE_RGUI:   return KEY_RIGHTWIN;
+
+#if defined(IOS)
+		case SDL_SCANCODE_AC_BACK:        return KEY_ESCAPE;
+#endif
+
 		default:                  break;
 	}
 
@@ -968,6 +974,61 @@ static void Impl_HandleJoystickButtonEvent(SDL_JoyButtonEvent evt, Uint32 type)
 	if (event.type != ev_console) D_PostEvent(&event);
 }
 
+#ifdef TOUCHINPUTS
+// Lactozilla: Android touch inputs
+static void Impl_HandleTouchEvent(SDL_TouchFingerEvent evt)
+{
+	float touchx = evt.x;
+	float touchy = evt.y;
+
+	INT32 screenx = -1;
+	INT32 screeny = -1;
+
+	if (touchx >= 0.0 && touchx <= 1.0)
+		screenx = touchx * BASEVIDWIDTH;
+	if (touchy >= 0.0 && touchy <= 1.0)
+		screeny = touchy * BASEVIDHEIGHT;
+
+#if defined(LOGCAT)
+	if (screenx != -1 && screeny != -1)
+		CONS_Printf("YAYYYY!!!!! x %d y %d %d %d\n", screenx, screeny, evt.fingerId, evt.touchId);
+
+	if (evt.type == SDL_FINGERMOTION)
+		CONS_Printf("Finger motion\n");
+	else if (evt.type == SDL_FINGERUP)
+		CONS_Printf("Finger up\n");
+	else if (evt.type == SDL_FINGERDOWN)
+		CONS_Printf("Finger down\n");
+#endif
+
+	if (evt.type == SDL_FINGERDOWN || evt.type == SDL_FINGERUP)
+	{
+		event_t event;
+		switch (gamestate)
+		{
+			case GS_LEVEL:
+				if (menuactive || promptactive)
+				{
+					event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
+					event.key = KEY_SPACE;
+				}
+				else
+				{
+					event.type = (evt.type == SDL_FINGERDOWN) ? ev_touchdown : ev_touchup;
+					event.x = screenx;
+					event.y = screeny;
+				}
+				break;
+			default:
+				event.type = (evt.type == SDL_FINGERDOWN) ? ev_keydown : ev_keyup;
+				event.key = KEY_ENTER;
+				break;
+		}
+		D_PostEvent(&event);
+	}
+}
+#endif
+
 
 
 void I_GetEvent(void)
@@ -1013,6 +1074,13 @@ void I_GetEvent(void)
 			case SDL_JOYAXISMOTION:
 				Impl_HandleJoystickAxisEvent(evt.jaxis);
 				break;
+#ifdef TOUCHINPUTS
+			case SDL_FINGERMOTION:
+			case SDL_FINGERDOWN:
+			case SDL_FINGERUP:
+				Impl_HandleTouchEvent(evt.tfinger);
+				break;
+#endif
 #if 0
 			case SDL_JOYHATMOTION:
 				Impl_HandleJoystickHatEvent(evt.jhat)

src/st_stuff.c
@@ -15,6 +15,7 @@
 
 #include "doomdef.h"
 #include "g_game.h"
+#include "g_input.h"
 #include "r_local.h"
 #include "p_local.h"
 #include "f_finale.h"
@@ -1255,6 +1256,211 @@ static void ST_drawInput(void)
 	if (!demosynced) // should always be last, so it doesn't push anything else around
 		V_DrawThinString(x, y, hudinfo[HUD_INPUT].f|((leveltime & 4) ? V_YELLOWMAP : V_REDMAP), "BAD DEMO!!");
 }
+
+#ifdef TOUCHINPUTS
+static void ST_drawTouchInput(void)
+{
+	fixed_t dupx = vid.dup*FRACUNIT;
+	fixed_t dupy = vid.dup*FRACUNIT;
+	const INT32 flags = V_NOSCALESTART;
+	const INT32 accent = (stplyr->skincolor ? skincolors[stplyr->skincolor-1].ramp[4] : 0);
+	const INT32 shadow = vid.dup;
+	INT32 col, offs;
+	INT32 base, ybase;
+	INT32 xslant, yslant;
+	INT32 udw;
+	INT32 i, j;
+
+	INT32 x = FixedMul(touch_dpad_x * FRACUNIT, dupx) / FRACUNIT;
+	INT32 y = FixedMul(touch_dpad_y * FRACUNIT, dupy) / FRACUNIT;
+	INT32 w = FixedMul(touch_dpad_w * FRACUNIT, dupx) / FRACUNIT;
+	INT32 h = FixedMul(touch_dpad_h * FRACUNIT, dupy) / FRACUNIT;
+
+	touchconfig_t *tleft = &touchconfig[GC_STRAFELEFT];
+	touchconfig_t *tright = &touchconfig[GC_STRAFERIGHT];
+	touchconfig_t *tup = &touchconfig[GC_FORWARD];
+	touchconfig_t *tdown = &touchconfig[GC_BACKWARD];
+
+	touchconfig_t *tjump = &touchconfig[GC_JUMP];
+	touchconfig_t *tspin = &touchconfig[GC_SPIN];
+
+	if (stplyr->powers[pw_carry] == CR_NIGHTSMODE)
+		y -= FixedMul(16 * FRACUNIT, dupy) / FRACUNIT;
+
+	if (F_GetPromptHideHud(y))
+		return;
+
+#define CENTERPOS \
+	if (vid.height != BASEVIDHEIGHT * vid.dup) \
+		y += (vid.height - (BASEVIDHEIGHT * vid.dup)); \
+
+	// O backing
+	CENTERPOS;
+	V_DrawFill(x, y-1, w, h, flags|20);
+	V_DrawFill(x, y+h-1, w, shadow, flags|29);
+
+	if (vid.dup == 1)
+		udw = 2;
+	else
+		udw = (vid.dup * 3);
+
+#define THISMACRONEEDSANAME(touch) \
+	x = FixedMul(touch->x * FRACUNIT, dupx) / FRACUNIT; \
+	y = FixedMul(touch->y * FRACUNIT, dupy) / FRACUNIT; \
+	w = FixedMul(touch->w * FRACUNIT, dupx) / FRACUNIT; \
+	h = FixedMul(touch->h * FRACUNIT, dupy) / FRACUNIT; \
+	xslant = FixedMul((touch->w/2) * FRACUNIT, dupx) / FRACUNIT; \
+	yslant = FixedMul((touch->h/2) * FRACUNIT, dupy) / FRACUNIT; \
+	CENTERPOS;
+
+	// <
+	THISMACRONEEDSANAME(tleft);
+
+	base = (w - xslant);
+	ybase = (y + h) - vid.dup;
+
+#define drawleftbutton(color, offset) \
+	V_DrawFill(x, y+offset, base+(vid.dup), h, color|flags); \
+	for (i = 0; i < xslant; i++) \
+		V_DrawFill(x+base+i+(vid.dup), (y+i)+offset, vid.dup, h-(i*2), color|flags);
+
+	if (stplyr->cmd.sidemove < 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawleftbutton(29, shadow);
+	}
+
+	drawleftbutton(col, offs);
+
+	// ^
+	THISMACRONEEDSANAME(tup);
+
+	yslant /= 2;
+	yslant += (vid.dup * 2) + 1;
+
+	base = w;
+	ybase = (y + h) - vid.dup;
+
+#define drawupbutton(color, offset) \
+	for (i = 0; i < yslant; i++) \
+		V_DrawFill(x+i, y+offset, 1, (h-yslant)+i, color|flags); \
+	ybase = (h-yslant)+i; \
+	V_DrawFill(x+i, y+offset, udw, ybase, color|flags); \
+	for (j = 0; j < yslant; j++) \
+		V_DrawFill(x+i+j+udw, y+offset, 1, (ybase-(j+1)), color|flags); \
+
+	if (stplyr->cmd.forwardmove > 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawupbutton(29, shadow);
+	}
+
+	drawupbutton(col, offs);
+
+	// >
+	THISMACRONEEDSANAME(tright);
+
+	base = (w - xslant);
+	ybase = (y + h) - vid.dup;
+
+#define drawrightbutton(color, offset) \
+	V_DrawFill(x+base-(vid.dup), y+offset, base+(vid.dup), h, color|flags); \
+	for (i = 0; i < xslant; i++) \
+		V_DrawFill(x+(base-(vid.dup))-i-(vid.dup), (y+i)+offset, vid.dup, h-(i*2), color|flags);
+
+	if (stplyr->cmd.sidemove > 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawrightbutton(29, shadow);
+	}
+
+	drawrightbutton(col, offs);
+
+	// v
+	THISMACRONEEDSANAME(tdown);
+
+	yslant /= 2;
+	yslant += (vid.dup * 2) + 1;
+
+	base = w;
+	ybase = (y + h);
+
+#define drawdownbutton(color, offset) \
+	for (i = 0; i < yslant; i++) \
+		V_DrawFill(x+i, (y+(yslant-i)) + offset, 1, (h-yslant)+i, color|flags); \
+	ybase = (h-yslant)+i; \
+	V_DrawFill(x+i, y+offset, udw, ybase, color|flags); \
+	for (j = 0; j < yslant; j++) \
+		V_DrawFill(x+i+j+udw, ((y+(yslant-i))+j) + 1 + offset, 1, (ybase-(j+1)), color|flags);
+
+	if (stplyr->cmd.forwardmove < 0)
+	{
+		col = accent;
+		offs = shadow;
+	}
+	else
+	{
+		col = 16;
+		offs = 0;
+		drawdownbutton(29, shadow);
+	}
+
+	drawdownbutton(col, offs);
+
+#undef CENTERPOS
+#define CENTERPOS \
+	if (vid.width != BASEVIDWIDTH * vid.dup) \
+		x += (vid.width - (BASEVIDWIDTH * vid.dup)); \
+	if (vid.height != BASEVIDHEIGHT * vid.dup) \
+		y += (vid.height - (BASEVIDHEIGHT * vid.dup)); \
+
+#define drawbutt(control, butt, symb) \
+	THISMACRONEEDSANAME(control); \
+	if (stplyr->cmd.buttons & butt) \
+	{ \
+		col = accent; \
+		offs = shadow; \
+	} \
+	else \
+	{ \
+		col = 16; \
+		offs = 0; \
+		V_DrawFill(x, y + h, w, shadow, 29|flags); \
+	} \
+	V_DrawFill(x, y + offs, w, h, col|flags); \
+	V_DrawCharacter((x + (w / 2)) - ((8*vid.dup) / 2), (y + (h / 2)) - ((8*vid.dup) / 2) + offs, symb|flags, false);
+
+	drawbutt(tjump, BT_JUMP, 'J');
+	drawbutt(tspin, BT_SPIN,  'S');
+
+#undef drawbutt
+#undef drawdownbutton
+#undef drawrightbutton
+#undef drawupbutton
+#undef drawleftbutton
+#undef THISMACRONEEDSANAME
+#undef CENTERPOS
+
+}
+#endif
 
 static boolean lt_active = false;
 
@@ -2845,6 +3051,10 @@ static void ST_overlayDrawer(void)
 
 	if ((cv_showinput.value && !players[displayplayer].spectator) || (modeattacking && !(demoplayback && hu_showscores)))
 		ST_drawInput();
+#ifdef TOUCHINPUTS
+	else
+		ST_drawTouchInput();
+#endif
 
 	ST_drawDebugInfo();
 }